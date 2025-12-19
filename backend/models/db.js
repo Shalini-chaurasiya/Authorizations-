4c7b49c5834957ba6df7bdd9c8340431a0416d62
Shalini-chaurasiya <shalinichaurasiya2203@gmail.com>
2025-12-19 11:00:28 +0530

    successfully created database MONGODB is connected

---
+++ backend/models/db.js
@@ -0,0 +1,13 @@
+
+
+ const mongoose = require('mongoose');
+
+const mongo_url = process.env.MONGODB_URI;
+
+mongoose.connect(mongo_url)
+  .then(() => {
+    console.log('MONGODB Connected...');
+  })
+  .catch((err) => {
+    console.log('MongoDB Connection Error:', err);
+  });
